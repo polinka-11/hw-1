2ec2b57ccdfa9b77a5863e786f35e1a5de47603c
polinka-11 <polinka-1_1@mail.ru>
2023-07-20 20:07:09 +0300

    hw-5

---
+++ hw-1/hw-5.js
@@ -0,0 +1,121 @@
+//const number = (a , b) => {  №1
+  //  if (a < b) {
+    //    return a;
+    //} else {
+     //   return b;
+    //}
+//}
+//console.log(number(8 , 4)) ;
+//console.log(number( 2, 10));
+
+
+//function number() {  //№2
+  //let a = prompt('Введите число');
+  //if (a % 2 == 0) {
+   // return alert('Число четное');
+  //} else {
+   // return alert('Число нечетное');
+  //}
+//}
+//number();
+
+//function num (number) {№3.1
+ // console.log(number ** 2);
+//}
+
+//num (5);
+//num (10);
+
+
+//function num (number) { //№3.2
+ // return(number ** 2);
+//}
+
+//console.log(num(2));
+//console.log(num(3));
+
+
+  //function age() { //№4
+  //let number = prompt('Сколько Вам лет?');
+  //if (number < 0) {
+   // alert("Вы ввели неверное значение");
+  //}
+  //if (number < 1 || number < 12) {
+   // alert("Привет, друг!");
+  //} 
+  //if (number ===13 || number > 14) {
+   // alert("Добро пожаловать!");
+  //} 
+  //}
+  //age();
+
+
+  //function number(a , b) {    //№5
+    //if (isNaN(a) || isNaN(b)) {
+     // return console.log('Одно или два значения не являются числами');
+    //} else {
+      //return console.log(a* b); 
+  //    }
+  //}
+  //number(4, 5);
+  //number('a', 's');
+
+
+  //function num() {  //№6
+    //let n = prompt('Введите число');
+      //  if (isNaN(n)) {
+      //return alert('Введенное значение, не является числом');
+    //}
+    //let result = n ** 3;
+    //if ((n)) {
+     // return alert(`${n} в кубе равняется ${result}`);
+    //}
+  //}
+  //num();
+
+
+  //function getArea() { №7
+   // return this.radius ** 2 *3.14;
+  //}
+  //function getPerimetr() {
+  //  return this.radius *2 *3.14;
+  //}
+  //const circle1 = {
+   // radius: 5,
+
+   // getArea: getArea,
+  //  getPerimetr: getPerimetr,
+  //}
+
+  //const circle2 = {
+   // radius: 3,
+
+   // getArea: getArea,
+   // getPerimetr: getPerimetr,
+ // }
+
+ // console.log(circle1.getArea());
+ // console.log(circle1.getPerimetr());
+  //console.log(circle2.getArea());
+ // console.log(circle2.getPerimetr());
+
+
+ //function monthNumber() {   //№8
+  //let num = prompt('Введите номер месяца'); 
+  //if (num == 1 || num == 2 || num == 12) {
+  //  return alert('Зима');
+ // }
+ // if (num == 3 ||num == 4 || num == 5) {
+ //   return alert('Весна');
+ // }
+ // if (num == 6 || num == 7 || num == 8) {
+ //   return alert('Лето');
+ // }
+ // if (num == 9 || num == 10 || num == 11) {
+  //  return alert('Осень');
+ // }
+ // if (num < 0 || num > 13) {
+ //   return alert('Такого месяца не существует, попробуй еще раз!');
+ // }
+ //}
+ 
