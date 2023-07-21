e4488346b061dd71e6b1ddd0a81a259875fe6019
polinka-11 <polinka-1_1@mail.ru>
2023-07-21 18:48:39 +0300

    draft hw5

--- hw-1/hw-5.js
+++ hw-1/hw-5.js
@@ -103,19 +103,19 @@
  //function monthNumber() {   //№8
   //let num = prompt('Введите номер месяца'); 
   //if (num == 1 || num == 2 || num == 12) {
-  //  return alert('Зима');
- // }
- // if (num == 3 ||num == 4 || num == 5) {
- //   return alert('Весна');
- // }
- // if (num == 6 || num == 7 || num == 8) {
- //   return alert('Лето');
- // }
- // if (num == 9 || num == 10 || num == 11) {
-  //  return alert('Осень');
- // }
- // if (num < 0 || num > 13) {
- //   return alert('Такого месяца не существует, попробуй еще раз!');
- // }
+   // return alert('Зима');
+  //}
+  //if (num == 3 ||num == 4 || num == 5) {
+   // return alert('Весна');
+  //}
+  //if (num == 6 || num == 7 || num == 8) {
+   // return alert('Лето');
+  //}
+  //if (num == 9 || num == 10 || num == 11) {
+   //return alert('Осень');
+  //}
+  //if (num < 0 || num > 13) {
+   // return alert('Такого месяца не существует, попробуй еще раз!');
+  //}
  //}
  
